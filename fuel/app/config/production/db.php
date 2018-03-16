edfb82f266c6c88b4047c2a5758174700e97383c
PC\USER <32762324+arthur-teleneco@users.noreply.github.com>
2018-03-16 14:40:43 +0900

    config/assets追加

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host=localhost;dbname=nhk-exam',
+			'username'   => getenv('DB_USERNAME'),
+			'password'   => getenv('DB_PASSWORD'),
 		),
 	),
 );
